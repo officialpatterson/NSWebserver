f47a528d713d6e8a1faa379ba76697d9dae651c3
Andrew Patterson <andrewrpatterson@btinternet.com>
2014-02-13 21:02:11 +0000

    working test

--- nswebserver.c
+++ nswebserver.c
@@ -1,33 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 #include <sys/types.h>
+#include <netinet/in.h>
 #include <sys/socket.h>
 
-int main(){
+int main(int argc, char *argv[])
+{
     
-
-
-    struct sockaddr_in addr;
-    addr.sin_addr.s_addr = INADDR_ANY;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(8080);
+    struct sockaddr_in sockaddr; /* socket info about our server */
+    int fd;            /* socket used to listen for incoming connections */
+    socklen_t socksize = sizeof(struct sockaddr_in);
     
+    sockaddr.sin_family = AF_INET;                /* set the type of connection to TCP/IP */
+    sockaddr.sin_addr.s_addr = INADDR_ANY; /* set our address to any interface */
+    sockaddr.sin_port = htons(8080);           /* set the server port number */
     
-    /*Create the socket necessary for listening*/
-    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    fd = socket(AF_INET, SOCK_STREAM, 0);
     
     if(fd == -1){
-        //error creating socket
+        //eror
     }
-    
-    //bind the socket to a port
-    if(bind(fd, (struct sockaddr *) &addr, sizeof(addr)) == -1){
+    /* bind serv information to mysocket */
+    if(bind(fd, (struct sockaddr *)&sockaddr, sizeof(struct sockaddr)) == -1){
         //error
     }
     
-    int backlog = 10;
-    
-    
-    if(listen(fd, backlog) == -1){
-        
-    }
+    /* start listening, allowing a queue of up to 1 pending connection */
+    listen(fd, 1);
+ 
+
     return 0;
 }
