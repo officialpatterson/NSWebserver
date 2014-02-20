4ac02e55a28b96f850cc734a65e8508126d7bbe2
officialpatterson <andrewrpatterson@btinternet.com>
2014-02-20 13:49:53 +0000

    able to access file resources

---
+++ src/resource.h
@@ -0,0 +1,9 @@
+/*file to allow fetching of server resources in an abstract manner*/
+#include <stdio.h>
+
+typedef struct resource Resource;
+
+Resource * createResource(char * location);
+char * getResourceData(Resource * r);
+int getResourceLength(Resource * r);
+void destroyResource(Resource * r);
